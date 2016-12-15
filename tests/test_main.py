32abdbf2a6ba8db6c4d396382792f6b9a71a8439
leonardo <leonardo.maccari@unitn.it>
2016-12-15 10:23:45 +0100

    removes CL input

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,13 @@
 #! /usr/bin/python
-import sys
 import mailbox
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
 import mbparse
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print 'Usage: /mbparse.py mbox names_dictionary'
-        sys.exit(1)
-
-    mbox_file = sys.argv[1]
-    dictionary = sys.argv[2]
+    mbox_file = "../testdata/wireless-12-2016.txt"
+    dictionary = "../testdata/aliases.xml"
     mailbox = mailbox.mbox(mbox_file)
     g = mbparse.parse_mbox_fragment(mailbox, dictionary)
     mbparse.get_ML_relevance(g)
